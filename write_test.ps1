81f7303d72747cac1638ec40b41e5ae8b31963d4
Vijay Tirumalai <31016667+vjtirumalai@users.noreply.github.com>
2017-12-12 14:02:31 -0500

    Update write_test.ps1

--- write_test.ps1
+++ write_test.ps1
@@ -1,2 +1,13 @@
-New-Item -Path . -Name "test.txt" -ItemType "file" -Value "Hello-World"
-Get-Content -Path . "test.txt"
+param([String]$operation="create")
+
+If ($operation -eq "delete") {
+  Write-Host $operation
+  Get-Content -Path . "test.txt"
+  Remove-Item -Path . -Name "test.txt"
+}
+
+If ($operation -eq "create") {
+  Write-Host $operation
+  New-Item -Path . -Name "test.txt" -ItemType "file" -Value "Hello-World"
+  Get-Content -Path . "test.txt"
+}
