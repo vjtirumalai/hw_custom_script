bcbe8acdf42b2d6fca33ec4c0e6dad1173fabdd5
Vijay Tirumalai <31016667+vjtirumalai@users.noreply.github.com>
2017-12-12 15:56:20 -0500

    Update write_test.ps1

--- write_test.ps1
+++ write_test.ps1
@@ -1,13 +1,14 @@
 param([String]$operation="create")
+$testFile="C:\test.txt"
 
 If ($operation -eq "delete") {
   Write-Host $operation
-  Get-Content -Path . "test.txt"
-  Remove-Item -Path . -Name "test.txt"
+  Get-Content -Path $testFile
+  Remove-Item -Path $testFile
 }
 
 If ($operation -eq "create") {
   Write-Host $operation
-  New-Item -Path . -Name "test.txt" -ItemType "file" -Value "Hello-World"
-  Get-Content -Path . "test.txt"
+  New-Item -Path $testFile -ItemType "file" -Value "Hello-World"
+  Get-Content -Path $testFile
 }
